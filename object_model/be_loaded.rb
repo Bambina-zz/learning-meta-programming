4f97e787206b6e6f14d0bb772f303972f3fccdff
廣野 まゆ子 <mayuko.hirono@gmail.com>
2016-02-14 13:24:13 +0900

    Refactor counter function.

--- object_model/be_loaded.rb
+++ object_model/be_loaded.rb
@@ -1,3 +1,4 @@
+
 def sayBoo
   p 'Loaded BOO! :P'
 end
@@ -5,17 +6,8 @@ end
 p File.basename(__FILE__) + ' is invoked'
 
 $i = 0
-def increment
-  lambda{ $i += 1 }
-end
-
-def printer
-  lambda{ p $i }
-end
-
 def counter
-  increment.call
-  printer.call
+  lambda{ p $i += 1 }.call
 end
 
 counter #=> 1
