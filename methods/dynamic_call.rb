777c170fd6a2165cf718f4418b296000f7da308f
廣野 まゆ子@SYS <hirono-mayuko@dmm.com>
2016-03-22 10:14:19 +0900

    Add sample code of send method.

---
+++ methods/dynamic_call.rb
@@ -0,0 +1,13 @@
+require 'pry'
+
+class MyClass
+  def my_method my_arg
+    my_arg *  2
+  end
+end
+
+binding.pry
+### TRY
+# obj = MyClass.new
+# obj.my_method 3 #=> 6
+# obj.send :my_method, 3 #=> 6
